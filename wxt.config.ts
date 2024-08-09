1cc083b2f852653938e65ae3d3c941e209b04977
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2024-08-09 15:58:10 -0300

    refactor: remove popup

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'wxt';
-import vuetify from 'vite-plugin-vuetify'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
   manifest: {
     permissions: ['webNavigation'],
     name: 'CrunchyComments'
-  },
-  vite: () => ({
-    plugins: [
-      vuetify({ autoImport: true })
-    ]
-  })
+  }
 });
