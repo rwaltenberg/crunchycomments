d6a98554a69925171573dda54823151713e6b4ef
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2024-08-12 09:25:53 -0300

    fix: make icons square

--- utils/icons.ts
+++ utils/icons.ts
@@ -1,15 +1,15 @@
 export const COLORED_ICONS = {
-  '16': '/icon/16.png',
-  '32': '/icon/32.png',
-  '48': '/icon/48.png',
-  '96': '/icon/96.png',
-  '128': '/icon/128.png'
+  '16': '/icon/icon16.png',
+  '32': '/icon/icon32.png',
+  '48': '/icon/icon48.png',
+  '96': '/icon/icon96.png',
+  '128': '/icon/icon.png'
 }
 
 export const GRAY_ICONS = {
-  '16': '/icon/16-grey.png',
-  '32': '/icon/32-grey.png',
-  '48': '/icon/48-grey.png',
-  '96': '/icon/96-grey.png',
-  '128': '/icon/128-grey.png'
+  '16': '/icon/gray16.png',
+  '32': '/icon/gray32.png',
+  '48': '/icon/gray48.png',
+  '96': '/icon/gray96.png',
+  '128': '/icon/gray.png'
 }
